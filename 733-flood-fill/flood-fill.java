8cec42f4c47441a12b7952ba43b93b36cd709e34
Somil <somilarora55@gmail.com>
2026-09-19 17:57:39 -0400

    Time: 0 ms (100.00%) | Memory: 46.4 MB (97.10%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int orignalColor = image[sr][sc];
+        if(orignalColor!=color){
+            dfs(image,sr,sc,orignalColor,color);
+        }
+        return image;
+    }
+    private void dfs(int[][] image, int r, int c, int originalColor, int newColor) {
+       
+        if (r < 0 || r >= image.length || c < 0 || c >= image[0].length || image[r][c] != originalColor) {
+            return; 
+        }
+        image[r][c] = newColor;
+        dfs(image, r - 1, c, originalColor, newColor); 
+        dfs(image, r + 1, c, originalColor, newColor); 
+        dfs(image, r, c - 1, originalColor, newColor); 
+        dfs(image, r, c + 1, originalColor, newColor); 
+    }
+}
